018eed3319b75c2d76c2e637495f8f9192056d79
richu54 <richuriz54@gmail.com>
2025-12-04 00:50:21 +0530

    Fix invalid URL crash

--- api/auth.js
+++ api/auth.js
@@ -1,20 +1,18 @@
-export default async function handler(req) {
-
-    const url = new URL(req.url);
-    const code = url.searchParams.get("code");
+export default async function handler(req, res) {
+    const code = req.query.code;
   
-    // STEP 1: Send user to GitHub login
+    // STEP 1: Redirect user to GitHub
     if (!code) {
-      return new Response(null, {
-        status: 302,
-        headers: {
-          Location: `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`
-        }
-      });
+      const githubURL =
+        "https://github.com/login/oauth/authorize" +
+        `?client_id=${process.env.GITHUB_CLIENT_ID}` +
+        "&scope=repo";
+  
+      return res.redirect(githubURL);
     }
   
-    // STEP 2: Exchange code for token
-    const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
+    // STEP 2: Exchange code for access token
+    const response = await fetch("https://github.com/login/oauth/access_token", {
       method: "POST",
       headers: {
         "Accept": "application/json",
@@ -27,17 +25,15 @@ export default async function handler(req) {
       })
     });
   
-    const data = await tokenRes.json();
+    const data = await response.json();
   
-    // STEP 3: Return token to Decap
-    return new Response(`
+    // STEP 3: Send token back to Decap CMS
+    res.setHeader("Content-Type", "text/html");
+    res.send(`
       <script>
         window.opener.postMessage(${JSON.stringify(data)}, "*");
         window.close();
       </script>
-    `, {
-      headers: { "Content-Type": "text/html" }
-    });
-  
+    `);
   }
   
