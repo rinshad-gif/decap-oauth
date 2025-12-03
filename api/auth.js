9fcd477cbc0644d4d46ab8900ea8dd5af9dc9ab5
richu54 <richuriz54@gmail.com>
2025-12-03 16:26:30 +0530

    Remove node-fetch and fix handler

--- api/auth.js
+++ api/auth.js
@@ -1,35 +1,32 @@
-import fetch from "node-fetch";
+import querystring from "querystring";
 
 export default async function handler(req, res) {
+  const params = querystring.parse(req.url.split("?")[1] || "");
 
-  const url = new URL(req.url, `https://${req.headers.host}`);
-  const code = url.searchParams.get("code");
-
-  // STEP 1: Redirect to GitHub Auth
-  if (!code) {
+  // STEP 1: Redirect to GitHub
+  if (!params.code) {
     const redirect = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`;
     res.writeHead(302, { Location: redirect });
     return res.end();
   }
 
-  // STEP 2: Exchange code for token
+  // STEP 2: Exchange code for access token
   const response = await fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
     headers: {
-      "Accept": "application/json",
+      Accept: "application/json",
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
       client_id: process.env.GITHUB_CLIENT_ID,
       client_secret: process.env.GITHUB_CLIENT_SECRET,
-      code
+      code: params.code
     })
   });
 
   const data = await response.json();
 
-  // STEP 3: Return token to Decap CMS
-  res.setHeader("Content-Type", "text/html");
+  // STEP 3: Send token back to Decap CMS
   res.end(`
     <script>
       window.opener.postMessage(${JSON.stringify(data)}, "*");
