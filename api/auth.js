0b8432089f55eb6f26d89d6a4780e4562c0104f9
richu54 <richuriz54@gmail.com>
2025-12-03 17:18:07 +0530

    Fix OAuth handler for Vercel

--- api/auth.js
+++ api/auth.js
@@ -1,36 +1,43 @@
-import querystring from "querystring";
+export default async function handler(req) {
 
-export default async function handler(req, res) {
-  const params = querystring.parse(req.url.split("?")[1] || "");
-
-  // STEP 1: Redirect to GitHub
-  if (!params.code) {
-    const redirect = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`;
-    res.writeHead(302, { Location: redirect });
-    return res.end();
+    const url = new URL(req.url);
+    const code = url.searchParams.get("code");
+  
+    // STEP 1: Send user to GitHub login
+    if (!code) {
+      return new Response(null, {
+        status: 302,
+        headers: {
+          Location: `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`
+        }
+      });
+    }
+  
+    // STEP 2: Exchange code for token
+    const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code
+      })
+    });
+  
+    const data = await tokenRes.json();
+  
+    // STEP 3: Return token to Decap
+    return new Response(`
+      <script>
+        window.opener.postMessage(${JSON.stringify(data)}, "*");
+        window.close();
+      </script>
+    `, {
+      headers: { "Content-Type": "text/html" }
+    });
+  
   }
-
-  // STEP 2: Exchange code for access token
-  const response = await fetch("https://github.com/login/oauth/access_token", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      client_id: process.env.GITHUB_CLIENT_ID,
-      client_secret: process.env.GITHUB_CLIENT_SECRET,
-      code: params.code
-    })
-  });
-
-  const data = await response.json();
-
-  // STEP 3: Send token back to Decap CMS
-  res.end(`
-    <script>
-      window.opener.postMessage(${JSON.stringify(data)}, "*");
-      window.close();
-    </script>
-  `);
-}
+  
