49dc671e65bb4534eb2310916c2ebb5a15510f69
richu54 <richuriz54@gmail.com>
2025-12-04 14:10:10 +0530

    vercel auth add to admin-onfig 4

--- api/auth.js
+++ api/auth.js
@@ -1,34 +1,39 @@
 export default async function handler(req, res) {
-    const code = req.query.code;
-  
-    // STEP 1: Redirect user to GitHub
-    if (!code) {
-      const githubURL =
-        "https://github.com/login/oauth/authorize" +
-        `?client_id=${process.env.GITHUB_CLIENT_ID}` +
-        "&scope=repo";
-  
-      return res.redirect(githubURL);
-    }
-  
-    // STEP 2: Exchange code for access token
-    const response = await fetch("https://github.com/login/oauth/access_token", {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET,
-        code
-      })
-    });
-  
-    const data = await response.json();
-  
-    // STEP 3: Send token back to Decap CMS
-    res.setHeader("Content-Type", "application/json");
-    res.send(data);
+  const { code } = req.query;
+
+  // STEP 1: Redirect to GitHub login
+  if (!code) {
+    const redirect = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`;
+    res.writeHead(302, { Location: redirect });
+    return res.end();
   }
-  
+
+  // STEP 2: Exchange code for token
+  const tokenResponse = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code,
+    }),
+  });
+
+  const data = await tokenResponse.json();
+
+  // STEP 3: Send token back to Decap (THIS WAS MISSING)
+  res.setHeader("Content-Type", "text/html");
+  res.end(`
+    <script>
+      if (window.opener) {
+        window.opener.postMessage(${JSON.stringify(data)}, "*");
+        window.close();
+      } else {
+        document.body.innerText = ${JSON.stringify(JSON.stringify(data, null, 2))};
+      }
+    </script>
+  `);
+}
