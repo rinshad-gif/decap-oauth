6fd5cc38ac8f844c850bf78a2c4edf170a57aedb
richu54 <richuriz54@gmail.com>
2025-12-04 15:20:38 +0530

    vercel auth add to admin-onfig 6

--- api/auth.js
+++ api/auth.js
@@ -1,15 +1,19 @@
-export default async function (req, res) {
-  const code = req.query.code;
+import fetch from "node-fetch";
 
+export default async function handler(req, res) {
+  const { code } = req.query;
+
+  // STEP 1: Redirect user to GitHub
   if (!code) {
-    const redirect = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`;
-    return res.redirect(redirect);
+    const authURL = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`;
+    return res.redirect(authURL);
   }
 
-  const tokenResponse = await fetch("https://github.com/login/oauth/access_token", {
+  // STEP 2: Exchange code for token
+  const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
     headers: {
-      Accept: "application/json",
+      "Accept": "application/json",
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
@@ -19,22 +23,14 @@ export default async function (req, res) {
     })
   });
 
-  const data = await tokenResponse.json();
+  const token = await tokenRes.json();
 
+  // STEP 3: Return result to Decap CMS
   res.setHeader("Content-Type", "text/html");
   res.end(`
-    <!DOCTYPE html>
-    <html>
-    <body>
-      <script>
-        if (window.opener) {
-          window.opener.postMessage(${JSON.stringify(data)}, "*");
-          window.close();
-        } else {
-          document.body.innerText = JSON.stringify(${JSON.stringify(data)}, null, 2);
-        }
-      </script>
-    </body>
-    </html>
+    <script>
+      window.opener.postMessage(${JSON.stringify(token)}, "*");
+      window.close();
+    </script>
   `);
 }
